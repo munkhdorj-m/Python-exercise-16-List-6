a0afee8ec74b68a7f10c8ab36db493181587b897
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-14 09:35:14 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -21,8 +21,8 @@ def test1(numbers, target_sum, expected):
     ([3, 8, 3, 5, 3, 7], 3),
     ([2, 3, 2, 4, 2, 2, 5], 2),
     ([1, 1, 1, 2, 2, 2, 2], 2),
-    ([1, 2, 3, 4, 5], 0),
-    ([6, 6, 7, 7, 8, 8, 9, 9], 0)
+    ([1, 2, 3, 4, 5], 1),
+    ([6, 6, 7, 7, 8, 8, 9, 9], 6)
 ])
 def test2(numbers, expected):
     assert find_majority_element(numbers) == expected
